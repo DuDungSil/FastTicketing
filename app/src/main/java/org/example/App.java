3ff949ba671ef32b41d204548c34dbf3036b25a8
DuDungSil <121501087+DuDungSil@users.noreply.github.com>
2025-05-08 21:07:04 +0900

    init

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        log.info("------------------------ Fast Ticketing Server Start ------------------------");
+        logger.info("------------------------ Fast Ticketing Server Start ------------------------");
     }
 }
